50695c9f4af04d632dcaceec98f3c8ce45b2e5bf
3xela <alexander.neagoe@gmail.com>
2024-01-23 21:19:39 -0500

    added put

--- monte_carlo.cpp
+++ monte_carlo.cpp
@@ -21,8 +21,20 @@ double monte_carlo_bs_call(double initial_stock, double strike_price,double vola
     return std::exp(-risk_free*maturity/days_in_a_year)*sum/sample_count;
 }
 
+double monte_carlo_bs_put(double initial_stock, double strike_price,double volatility, double maturity, double risk_free, int sample_count){
+    double sum = 0.0;
+    for (int i=0; i<sample_count; i++)
+    {
+        double sample = distribution(gen); 
+        double final_stock_price = initial_stock*std::exp( (risk_free  - std::pow(volatility, 2)/ 2)*maturity/days_in_a_year  + volatility*sample*std::sqrt(maturity/days_in_a_year)  );
+        sum += std::max(strike_price - final_stock_price, 0.0);
+    }
+    return std::exp(-risk_free*maturity/days_in_a_year)*sum/sample_count;
+}
+
 
 int main(){
-    std::cout << monte_carlo_bs_call(60, 65, 0.3 ,0.25*days_in_a_year, 0.08 , 10000000) << std::endl;
+    std::cout << monte_carlo_bs_call(60, 65, 0.3 ,0.25*days_in_a_year, 0.08 , 10000001) << std::endl;
+    std::cout << monte_carlo_bs_put(60, 65, 0.3 ,0.25*days_in_a_year, 0.08 , 10000001) << std::endl;
     return 0;
 }
